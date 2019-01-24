3b447e0ed817e462a2b0c13f460494561aa3edd3
Brian LeBlanc <bleblan2@unb.ca>
2019-01-23 23:56:23 -0400

    Build configparser structure in config class

--- aws_lp/config.py
+++ aws_lp/config.py
@@ -1,18 +1,39 @@
 """Configuration handler class."""
+import os
+
+from six.moves import configparser
 
 
 class Config(object):
     """Configuration handler class."""
 
     def __init__(self):
-        # TODO bring in config parser
-        self.config = {}
-        # TODO add section names for different profiles
+        self.config_file_name = 'aws_lp'
+        self.config_section = 'default'
+
+        if os.path.isdir(os.path.expanduser('~/.config')):
+            self.config_file = os.path.expanduser(
+                '~/.config/' + self.config_file_name + '/config')
+        else:
+            self.config_file = os.path.expanduser('~/.' + self.config_file_name)
+
+        self.configparser = configparser.ConfigParser()
 
     def set_config(self, **kwargs):
         """Update config values."""
-        self.config.update(kwargs)
+        # TODO Update config file
+        # self.config.update(kwargs)
 
     def get_config(self):
         """Return stored config."""
-        return self.config
+        if not os.path.isfile(self.config_file):
+            return {}
+
+        self.configparser.read(self.config_file)
+
+        try:
+            config = dict(self.configparser.items(self.config_section))
+        except configparser.NoSectionError:
+            return {}
+
+        return config
